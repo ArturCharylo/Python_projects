a4c3f8fa08bda1bd1d2a561bd8f6726dbc3481e5
takojakos <dziki.sledz@op.pl>
2025-07-25 13:48:17 +0200

    Added Breadth First Search

--- algorythms/DFS.py
+++ algorythms/DFS.py
@@ -20,13 +20,26 @@ class Graph:
                 self.dfs_util(neighbour, visited)
 
     def dfs(self, v):
-
-        # Create a set to store visited vertices
         visited = set()
 
-        # Call the recursive helper function
-        # to print DFS traversal
+        # recursive helper function
         self.dfs_util(v, visited)
+
+    def bfs(self, s):
+        visited = [False] * (max(self.graph) + 1)
+
+        queue = []
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            for i in self.graph[s]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
 
 
 if __name__ == "__main__":
@@ -41,3 +54,6 @@ if __name__ == "__main__":
     print("Following is Depth First Traversal (starting from vertex 2)")
 
     g.dfs(2)
+
+    print("\nFollowing is Breadth First Traversal (starting from vertex 2)")
+    g.bfs(2)
